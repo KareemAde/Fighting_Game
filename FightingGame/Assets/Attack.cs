e37ce7dd648bc7aef05dab77d3f57becea21ad6c
Kareem Adebisi <k1adebisi@ascellon.com>
2020-10-02 15:02:04 -0400

    Initial Commit

---
+++ FightingGame/Assets/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public bool isHit;
+    public bool isBlocked;
+
+    public int attackDamage;
+    public int blockedDamage;
+    public int frameCounter;
+    public int hitStun;
+    public int blockStun;
+    public int totalFrames;
+
+    enum hitType
+    {
+        Hit,
+        Grab
+    };
+
+    public Vector2 launchAngle;
+
+}
---
+++ FightingGame/Assets/Fighter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{ // Fighter State Checks
+
+    public bool isHit;
+    public bool isCrouching;
+    public bool isAir;
+    public bool isBlocking;
+    public bool isAttacking;
+    public bool isFrameCancellable;
+    public enum attckDirection
+    {
+        High,
+        Mid,
+        Low,
+        Overhead
+    };
+
+    public attckDirection AttackDirection;
+
+    public int attackFrame;
+
+    public float walkSpeed = 12f;
+    public float backWalkSpeed;
+    public float runSpeed;
+    public float jumpHeight;
+
+    public bool Function()
+    {
+        return isHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attckDirection.High.Equals(0))
+        {
+
+        }
+    }
+}
